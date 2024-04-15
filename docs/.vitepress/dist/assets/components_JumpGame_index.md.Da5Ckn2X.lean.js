e6afbbcdb2677377f4a5793f1db8ab227ae0a0e3
hfdy0935 <hfdy09354121794@gmail.com>
2024-04-15 15:33:07 +0800

    u

--- docs/.vitepress/dist/assets/components_JumpGame_index.md.Da5Ckn2X.lean.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/chunks/index.0_sFAP7r.js","assets/chunks/framework.CL7hJqEm.js","assets/chunks/theme.D2XKSrvV.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{l as t,S as a,D as n,x as o,R as m,av as s,aq as r}from"./chunks/framework.CL7hJqEm.js";const c=JSON.parse('{"title":"","description":"","frontmatter":{"layout":false},"headers":[],"relativePath":"components/JumpGame/index.md","filePath":"components/JumpGame/index.md","lastUpdated":null}'),p={name:"components/JumpGame/index.md"},u=t({...p,setup(_){const e=s(()=>r(()=>import("./chunks/index.0_sFAP7r.js"),__vite__mapDeps([0,1,2])));return(d,l)=>(m(),a("div",null,[n(o(e))]))}});export{c as __pageData,u as default};
